e24ad268241ff717cbb74ef02b9e726483ba7231
icvotria <icvotria@gmail.com>
2022-03-01 17:46:04 +0000

    pinkman cant go out of bounds

--- src/main.js
+++ src/main.js
@@ -25,14 +25,23 @@ function preload() {
   this.load.image('pinkman', '/sprites/pinkman.png');
   this.load.image('pubWoman', '/sprites/pub.jpeg');
   this.load.image('base_tiles', '/tiles/tileset.png')
-	this.load.tilemapTiledJSON('tilemap', '/tiles/map_01.json')
+	this.load.tilemapTiledJSON('tilemap', '/tiles/map.json')
     
 }
 
 function create () {
 
-  this.pub = this.add.image(400, 300, 'pubWoman');
+  const map = this.make.tilemap({ key: 'tilemap' })
+	const tileset = map.addTilesetImage('tileset', 'base_tiles')
+	
+	this.layer1 = map.createStaticLayer('grass', tileset)
+	this.layer2 = map.createStaticLayer('walls', tileset)
+  
+  map.setCollisionBetween(1, 999, true, 'walls'); 
+
   this.hero = this.add.image(400, 300, 'pinkman');
+  this.physics.add.existing(this.hero);
+  this.hero.body.setCollideWorldBounds(true, 1, 1);
 
   this.cursors = this.input.keyboard.createCursorKeys();
 }
