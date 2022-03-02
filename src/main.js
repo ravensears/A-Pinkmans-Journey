e70d980c644c547eeb9cd6f1ae91aa805eb32d2d
icvotria <icvotria@gmail.com>
2022-03-02 11:25:15 +0000

    music!!

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ var config = {
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 0 }
+      gravity: { y: 0 },
     }
   }
 };
@@ -24,33 +24,50 @@ function preload() {
     
   this.load.image('pinkman', '/sprites/pinkman.png');
   this.load.image('pubWoman', '/sprites/pub.jpeg');
-  this.load.image('base_tiles', '/tiles/tileset.png')
-	this.load.tilemapTiledJSON('tilemap', '/tiles/map.json')
+  this.load.image('base_tiles', '/tiles/tileset.png');
+	this.load.tilemapTiledJSON('tilemap', '/tiles/map.json');
+  this.load.audio('bens_beautiful_song', '/audio/music_2.mp3');
     
 }
 
 function create () {
 
-  const map = this.make.tilemap({ key: 'tilemap' })
-	const tileset = map.addTilesetImage('tileset', 'base_tiles')
-	
-	this.layer1 = map.createStaticLayer('grass', tileset)
-	this.layer2 = map.createStaticLayer('walls', tileset)
-  
-  map.setCollisionBetween(1, 999, true, 'walls'); 
+  this.music = this.sound.add("bens_beautiful_song");
 
-  this.hero = this.add.image(400, 300, 'pinkman');
-  this.physics.add.existing(this.hero);
+  var musicConfig = {
+    mute: false,
+    volume: 1,
+    rate: 1,
+    detune: 0,
+    seek: 0,
+    loop: true,
+    delay: 0
+  }
+
+  this.music.play(config);
+
+  const map = this.make.tilemap({ key: 'tilemap' });
+	const tileset = map.addTilesetImage('tileset', 'base_tiles');
+	
+	const layer1 = map.createStaticLayer('grass', tileset);
+	const layer2 = map.createStaticLayer('walls', tileset);
+  // layer2.setCollisionByProperty({ collides: true });
+
+  this.hero = this.physics.add.sprite(400, 300, 'pinkman');
+
+  // this.physics.add.existing(this.hero);
+  // this.physics.add.existing(layer2);
+  // this.physics.add.collider(this.hero, layer2);
+  
   this.hero.body.setCollideWorldBounds(true, 1, 1);
 
   this.cursors = this.input.keyboard.createCursorKeys();
+
 }
 
 function update () {
 
   if (this.cursors.up.isDown) {
-    console.log(this.hero.x);
-    console.log(this.hero.y);
     this.hero.y -= 2;
   } else if (this.cursors.down.isDown) {
     this.hero.y += 2;
