91d3f8be09ac9da9842dff39aa85909bd80e81dd
icvotria <icvotria@gmail.com>
2022-03-02 13:52:29 +0000

    animation preliminaries and wall bumping!

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ var config = {
   height: 720,
   renderer: Phaser.AUTO,
   backgroundColor: '#FD8379',
-  
   scene: {
       preload: preload,
       create: create,
@@ -13,7 +12,7 @@ var config = {
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 0 },
+      gravity: { y: 0 }
     }
   }
 };
@@ -21,22 +20,18 @@ var config = {
 const Game = new Phaser.Game(config);
 
 function preload() {
-    
-  this.load.image('pinkman', '/sprites/pinkman.png');
-  this.load.image('pubWoman', '/sprites/pub.jpeg');
-  this.load.image('base_tiles', '/tiles/tileset.png');
-	this.load.tilemapTiledJSON('tilemap', '/tiles/map.json');
-  this.load.audio('bens_beautiful_song', '/audio/music_2.mp3');
-    
+    this.load.spritesheet('pinkman', '/sprites/pinkman.png', { frameWidth: 32, frameHeight: 32 });
+    this.load.image('base_tiles', '/tiles/tileset.png');
+	  this.load.tilemapTiledJSON('tilemap', '/tiles/map.json');
+    this.load.audio('bens_beautiful_song', '/audio/music_2.mp3');
 }
 
 function create () {
-
   this.music = this.sound.add("bens_beautiful_song");
 
   var musicConfig = {
     mute: false,
-    volume: 1,
+    volume: 0.5,
     rate: 1,
     detune: 0,
     seek: 0,
@@ -48,32 +43,49 @@ function create () {
 
   const map = this.make.tilemap({ key: 'tilemap' });
 	const tileset = map.addTilesetImage('tileset', 'base_tiles');
-	
-	const layer1 = map.createStaticLayer('grass', tileset);
-	const layer2 = map.createStaticLayer('walls', tileset);
-  // layer2.setCollisionByProperty({ collides: true });
+
+  const layer1 = map.createStaticLayer('grass', tileset);
+  const layer2 = map.createStaticLayer('walls', tileset);
+
+  layer2.setCollisionByProperty({ collides: true });
 
   this.hero = this.physics.add.sprite(400, 300, 'pinkman');
+  this.hero2 = this.physics.add.sprite(300, 200, 'pinkman');
 
-  // this.physics.add.existing(this.hero);
-  // this.physics.add.existing(layer2);
-  // this.physics.add.collider(this.hero, layer2);
-  
-  this.hero.body.setCollideWorldBounds(true, 1, 1);
+  this.physics.add.collider(this.hero, layer2);
+  this.physics.add.collider(this.hero2, layer2);
 
   this.cursors = this.input.keyboard.createCursorKeys();
 
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('pinkman', { start: 0, end: 11 }),
+    frameRate: 10,
+    repeat: -1
+  });
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('pinkman', { start: 0, end: 11 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  this.physics.add.collider(this.hero, this.hero2);
 }
 
 function update () {
-
+  this.hero.setVelocity(0)
   if (this.cursors.up.isDown) {
-    this.hero.y -= 2;
+    this.hero.setVelocityY(-160);
+    this.hero.anims.play('right', true);
   } else if (this.cursors.down.isDown) {
-    this.hero.y += 2;
+    this.hero.setVelocityY(160);
+    this.hero.anims.play('right', true);
   } else if (this.cursors.right.isDown) {
-    this.hero.x += 2;
+    this.hero.setVelocityX(160);
+    this.hero.anims.play('right', true);
   } else if (this.cursors.left.isDown) {
-    this.hero.x -= 2;
+    this.hero.setVelocityX(-160);
+    this.hero.anims.play('right', true);
   }
 }
