c1aaec61354fc9b133a2b6b252e1cc78ed607394
Ben Coyne <bencoyne123@yahoo.co.uk>
2022-03-03 16:12:43 +0000

    wip implementing treasure finding

--- src/main.js
+++ src/main.js
@@ -1,147 +1,201 @@
 const config = {
-  type: Phaser.AUTO,
-  //width: 1080,
-  //height: 720,
-  //renderer: Phaser.AUTO,
-  parent: 'phaser-game',
-  scale: {
-    parent: 'phaser-game',
-    mode: Phaser.DOM.FIT,
-    width: window.innerWidth,
-    height: window.innerHeight
-  },
-  scene: {
-      preload: preload,
-      create: create,
-      update: update
-  },
-  physics: {
-    default: 'arcade',
-    arcade: {
-      debug: true
-    }
-  }
+	type: Phaser.AUTO,
+	//width: 1080,
+	//height: 720,
+	//renderer: Phaser.AUTO,
+	parent: "phaser-game",
+	scale: {
+		parent: "phaser-game",
+		mode: Phaser.DOM.FIT,
+		width: window.innerWidth,
+		height: window.innerHeight,
+	},
+	scene: {
+		preload: preload,
+		create: create,
+		update: update,
+	},
+	physics: {
+		default: "arcade",
+		arcade: {
+			debug: true,
+		},
+	},
 };
 
 const game = new Phaser.Game(config);
 
 // ************PRELOAD****************
 function preload() {
-    this.load.spritesheet('pinkman', '/sprites/pinkman_run.png', { frameWidth: 32, frameHeight: 32 });
-    this.load.spritesheet('sadGuy', '/sprites/pien.png', { frameWidth: 32, frameHeight: 32 });
-    this.load.image('base_tiles', '/tiles/tileset.png');
-	  this.load.tilemapTiledJSON('tilemap', '/tiles/map.json');
-    this.load.audio('bens_beautiful_song', '/audio/music_2.mp3');
-    this.load.image('object', '/sprites/pinkman.png')
+	this.load.spritesheet("pinkman", "/sprites/pinkman_run.png", {
+		frameWidth: 32,
+		frameHeight: 32,
+	});
+	this.load.spritesheet("sadGuy", "/sprites/pien.png", {
+		frameWidth: 32,
+		frameHeight: 32,
+	});
+	this.load.image("base_tiles", "/tiles/tileset.png");
+	this.load.tilemapTiledJSON("tilemap", "/tiles/map.json");
+	this.load.audio("bens_beautiful_song", "/audio/music_2.mp3");
+	this.load.audio("beep", "/audio/beep.mp3");
+	this.load.image("object", "/sprites/pinkman.png");
 }
 
 // ************CREATE****************
-function create () {
-  this.music = this.sound.add("bens_beautiful_song");
+function create() {
+	this.music = this.sound.add("bens_beautiful_song");
 
-  var musicConfig = {
-    mute: false,
-    volume: 0.3,
-    rate: 1,
-    detune: 0,
-    seek: 0,
-    loop: true,
-    delay: 0
-  }
+	var musicConfig = {
+		mute: true,
+		volume: 0.3,
+		rate: 1,
+		detune: 0,
+		seek: 0,
+		loop: true,
+		delay: 0,
+	};
 
-  this.music.play(musicConfig);
+	this.music.play(musicConfig);
 
-  // mute = game.add.sprite(game.world.centreX, game.word.centreY, 'greenie');
-  // mute.anchor.set(0.5);
-  // game.input.onDown.add(removeMusic, this)
+	// mute = game.add.sprite(game.world.centreX, game.word.centreY, 'greenie');
+	// mute.anchor.set(0.5);
+	// game.input.onDown.add(removeMusic, this)
 
-  const map = this.make.tilemap({ key: 'tilemap' }); 
-	const tileset = map.addTilesetImage('tileset', 'base_tiles');
-  
-  const layer1 = map.createStaticLayer('grass', tileset);
-  const layer2 = map.createStaticLayer('walls', tileset);
+	const map = this.make.tilemap({ key: "tilemap" });
+	const tileset = map.addTilesetImage("tileset", "base_tiles");
 
-  layer2.setCollisionByProperty({ collides: true });
+	const layer1 = map.createStaticLayer("grass", tileset);
+	const layer2 = map.createStaticLayer("walls", tileset);
 
-  this.hero = this.physics.add.sprite(400, 300, 'sadGuy').setScale(1.5);
-  this.hero.setOrigin(0.5, 0.5);
-  this.hero2 = this.physics.add.sprite(300, 200, 'pinkman');
-  object = this.add.image(450, 350, 'object').setInteractive();
+	layer2.setCollisionByProperty({ collides: true });
 
-  score = 0;
+	this.hero = this.physics.add.sprite(400, 300, "sadGuy").setScale(1.5);
+	this.hero.setOrigin(0.5, 0.5);
+	this.hero2 = this.physics.add.sprite(300, 200, "pinkman");
+	object = this.add.image(450, 350, "object").setInteractive();
 
-  object.on('pointerdown', function (pointer) {
-    score += 10
+	this.r4 = this.add.rectangle(368, 192, 30, 63, "00FFFFFF");
+	this.treasure = this.physics.add.existing(this.r4, 1);
+	this.treasure.visible = false;
 
-    console.log(`${score}`)
+	this.physics.add.collider(this.treasure, this.hero);
 
-  });
+	score = 0;
 
-  this.physics.add.collider(this.hero, layer2);
-  this.physics.add.collider(this.hero2, layer2);
+	object.on("pointerdown", function (pointer) {
+		score += 10;
 
-  this.cursors = this.input.keyboard.createCursorKeys();
+		console.log(`${score}`);
+	});
 
-  keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-  keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
-  keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-  keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+	this.physics.add.collider(this.hero, layer2);
+	this.physics.add.collider(this.hero2, layer2);
 
-  this.anims.create({
-    key: 'right',
-    frames: this.anims.generateFrameNumbers('sadGuy', { start: 6, end: 8 }),
-    frameRate: 10,
-    repeat: -1
-  });
-  this.anims.create({
-    key: 'left',
-    frames: this.anims.generateFrameNumbers('sadGuy', { start: 3, end: 5 }),
-    frameRate: 10,
-    repeat: -1    
-  });
-  this.anims.create({
-    key: 'top',
-    frames: this.anims.generateFrameNumbers('sadGuy', { start: 9, end: 11 }),
-    frameRate: 10,
-    repeat: -1
-  });
-  this.anims.create({
-    key: 'down',
-    frames: this.anims.generateFrameNumbers('sadGuy', { start: 0, end: 2 }),
-    frameRate: 10,
-    repeat: -1
-  });
-  this.anims.create({
-    key: 'idle',
-    frames: [ { key: 'sadGuy', frame: 1 } ],
-    frameRate: 10
-  });
+	this.cursors = this.input.keyboard.createCursorKeys();
 
-  this.physics.add.collider(this.hero, this.hero2);
-}
+	keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+	keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+	keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+	keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
 
-// ************UPDATE****************
-function update () {
-  this.hero.setVelocity(0)
-  if (this.cursors.up.isDown || keyW.isDown) {
-    this.hero.setVelocityY(-160);
-    this.hero.anims.play('top', true);
-  } else if (this.cursors.down.isDown || keyS.isDown) {
-    this.hero.setVelocityY(160);
-    this.hero.anims.play('down', true);
-  } else if (this.cursors.right.isDown || keyD.isDown) {
-    this.hero.setVelocityX(160);
-    this.hero.anims.play('right', true);
-  } else if (this.cursors.left.isDown || keyA.isDown) {
-    this.hero.setVelocityX(-160);
-    this.hero.anims.play('left', true);
-  } else {
-    this.hero.setVelocity(0)
-    this.hero.anims.play('idle', true);
-  }
+	this.anims.create({
+		key: "right",
+		frames: this.anims.generateFrameNumbers("sadGuy", { start: 6, end: 8 }),
+		frameRate: 10,
+		repeat: -1,
+	});
+	this.anims.create({
+		key: "left",
+		frames: this.anims.generateFrameNumbers("sadGuy", { start: 3, end: 5 }),
+		frameRate: 10,
+		repeat: -1,
+	});
+	this.anims.create({
+		key: "top",
+		frames: this.anims.generateFrameNumbers("sadGuy", { start: 9, end: 11 }),
+		frameRate: 10,
+		repeat: -1,
+	});
+	this.anims.create({
+		key: "down",
+		frames: this.anims.generateFrameNumbers("sadGuy", { start: 0, end: 2 }),
+		frameRate: 10,
+		repeat: -1,
+	});
+	this.anims.create({
+		key: "idle",
+		frames: [{ key: "sadGuy", frame: 1 }],
+		frameRate: 10,
+	});
 
-  scoreText = this.add.text(790, 0, `Treasures: ${score}`, {
+	// this.physics.add.collider(this.hero, this.hero2);
+
+	scoreText = this.add.text(790, 0, `Treasures: ${score}`, {
 		fontSize: "32px",
 		fill: "#ffffff",
 	});
+
+	// Get key object
+
+	// this.physics.add.collider(this.hero2, this.hero, () => {
+	// 	if (this.keyObj.isDown) {
+	// 		console.log("OUCH!");
+	// 		score += 1;
+	// 		sfx.play();
+	// 	}
+	// });
+	setTimeout(() => {
+		console.log(this.treasure.body.touching.none);
+	}, 5000);
+
+	setTimeout(() => {
+		console.log(this.treasure.body.touching.none);
+	}, 2000);
 }
+
+// ************UPDATE****************
+function update() {
+	scoreText.setText(`Treasures: ${score}`);
+	this.hero.setVelocity(0);
+	if (this.cursors.up.isDown || keyW.isDown) {
+		this.hero.setVelocityY(-160);
+		this.hero.anims.play("top", true);
+	} else if (this.cursors.down.isDown || keyS.isDown) {
+		this.hero.setVelocityY(160);
+		this.hero.anims.play("down", true);
+	} else if (this.cursors.right.isDown || keyD.isDown) {
+		this.hero.setVelocityX(160);
+		this.hero.anims.play("right", true);
+	} else if (this.cursors.left.isDown || keyA.isDown) {
+		this.hero.setVelocityX(-160);
+		this.hero.anims.play("left", true);
+	} else {
+		this.hero.setVelocity(0);
+		this.hero.anims.play("idle", true);
+	}
+
+	this.keyObj = this.input.keyboard.addKey("E");
+	const sfx = this.sound.add("beep");
+
+	if (this.treasure.active) {
+		if (this.treasure.body.touching.none === false && this.keyObj.isDown) {
+			console.log("You found the treasure!");
+			score += 1;
+			sfx.play();
+			this.treasure.setActive(false);
+		}
+	}
+}
+
+// currently always finds treasure once treasure touched
+
+// let hero3 = this.physics.add.sprite(
+// 	Math.random() * 1000,
+// 	Math.random() * 1000,
+// 	"pinkman"
+// );
+// hero3.setVelocityX(Math.random() * 1000);
+
+// this.physics.add.collider(hero3, this.hero);
+// this.physics.add.collider(hero3, layer2);
