feee3be9a421bec50d6fe6a0997599ad86fc3d4b
Francisco Pottinger-Marques <fpottinger.marques@gmail.com>
2022-03-03 16:41:20 +0000

    MongoDB test can create users

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ require('dotenv').config()
 
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
 const mongo = require('mongodb');
+
+const User = require('./models/user');
 const app = express();
-const db = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.smmbi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const db = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.smmbi.mongodb.net/lonelyDev?retryWrites=true&w=majority`
+
+mongoose.connect(db)
+  .then(result => app.listen(PORT))
+  .catch((err) => console.log('error'));
+
 const PORT = 3000;
 
 app.use(cors())
@@ -15,4 +24,17 @@ app.get('/', (_req, res) => {
   res.sendFile(__dirname + '/index.html')
 });
 
-app.listen(PORT);
+app.get('/username', (req, res) => {
+  const user = new User({
+    username: 'angryGuy',
+  });
+
+  user.save()
+  .then(result => {
+    res.send(result)
+  })
+  .catch(err => {
+    console.log(err);
+  });
+})
+
