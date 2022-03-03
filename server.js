c71b46bfb5b0502a56c708a593127f0f5bf72cb0
Francisco Pottinger-Marques <fpottinger.marques@gmail.com>
2022-03-03 14:01:38 +0000

    added mongo db

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
+require('dotenv').config()
+
 const express = require('express');
 const cors = require('cors');
+const mongo = require('mongodb');
 const app = express();
+const db = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.smmbi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const PORT = 3000;
 
 app.use(cors())
-
 app.use(express.json());
-
 app.use(express.static('src'));
 
 app.get('/', (_req, res) => {
