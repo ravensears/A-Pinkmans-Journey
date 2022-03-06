936dadf1be690cd11bcce490c569ee713658435a
Ben Coyne <bencoyne123@yahoo.co.uk>
2022-03-06 17:36:12 +0000

    animation stop when stationary

--- src/Player.js
+++ src/Player.js
@@ -1,57 +1,57 @@
 class Player extends Phaser.GameObjects.Sprite {
-  constructor(scene, x, y, sprite) {
-    super(scene, x , y, sprite);
+	constructor(scene, x, y, sprite) {
+		super(scene, x, y, sprite);
 
-    scene.add.existing(this);
-    scene.physics.world.enableBody(this);
+		scene.add.existing(this);
+		scene.physics.world.enableBody(this);
 
-    this.cursors = scene.input.keyboard.createCursorKeys();
-    this.keyA = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-    this.keyS = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
-    this.keyD = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-    this.keyW = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
-  }
+		this.cursors = scene.input.keyboard.createCursorKeys();
+		this.keyA = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+		this.keyS = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+		this.keyD = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+		this.keyW = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+	}
 
-  updatePlayer() {
-    this.body.setVelocity(0);
+	updatePlayer() {
+		this.body.setVelocity(0);
 
-    if (this.cursors.up.isDown || this.keyW.isDown) {
-      this.moveUp()
-    } else if (this.cursors.down.isDown || this.keyS.isDown) {
-      this.moveDown()
-    } else if (this.cursors.right.isDown || this.keyD.isDown) {
-      this.moveRight()
-    } else if (this.cursors.left.isDown || this.keyA.isDown) {
-      this.moveLeft()
-    } 
-    
-  }
+		if (this.cursors.up.isDown || this.keyW.isDown) {
+			this.moveUp();
+		} else if (this.cursors.down.isDown || this.keyS.isDown) {
+			this.moveDown();
+		} else if (this.cursors.right.isDown || this.keyD.isDown) {
+			this.moveRight();
+		} else if (this.cursors.left.isDown || this.keyA.isDown) {
+			this.moveLeft();
+		} else {
+			this.play(false);
+		}
+	}
 
-  updateHand(player) {
-    this.x = player.x
-    this.y = player.y
-  }
+	updateHand(player) {
+		this.x = player.x;
+		this.y = player.y;
+	}
 
-  moveUp() {
-    this.body.setVelocityY(-160);
-    this.play("top", true);
-  }
+	moveUp() {
+		this.body.setVelocityY(-180);
+		this.play("top", true);
+	}
 
-  moveDown() {
-    this.body.setVelocityY(160);
-    this.play("down", true);
-  }
+	moveDown() {
+		this.body.setVelocityY(180);
+		this.play("down", true);
+	}
 
-  moveLeft() {
-    this.body.setVelocityX(-160);
-    this.play("left", true);
-  }
+	moveLeft() {
+		this.body.setVelocityX(-180);
+		this.play("left", true);
+	}
 
-  moveRight() {
-      this.body.setVelocityX(160);
-      this.play("right", true);
-  }
+	moveRight() {
+		this.body.setVelocityX(180);
+		this.play("right", true);
+	}
 }
 
 export default Player;
-
